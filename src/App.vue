9773ae5f9a4153988c9474e51643de01a3d3561e
Aragao <epicappcode@gmail.com>
2024-07-10 14:02:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container d-flex flex-column align-items-center">
+    <h1>Next Experiênce</h1>
+    <div class="video">
+      <div class="container">
+        <iframe src="https://player.vimeo.com/video/982043933?h=d233ffd639" width="1140" height="641.25" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+    <br>
+    <a href="https://wa.me/557998304742"><button type="button" class="btn btn-success btn-lg">Whatsapp</button></a>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: white;
+  margin-top: 30px;
+  font-size: 60px;
+}
+
+</style>
